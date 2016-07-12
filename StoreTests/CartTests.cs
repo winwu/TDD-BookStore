e416a3b386628a7c350e443f011d8992246d2956
winwu <yiyingwu.1990@gmail.com>
2016-07-12 18:52:11 +0800

    [red] 描述第四個測試案例 測試失敗

--- StoreTests/CartTests.cs
+++ StoreTests/CartTests.cs
@@ -78,5 +78,28 @@ namespace Store.Tests
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void Get_Total_Price_buy_book_id_1_and_2_and_3_and_4()
+        {
+            //  第一二三四集各買一本  價格應為 100*4*0.8 = 320
+            // arrange
+            var books = new List<Book>
+            {
+                new Book() { Id = "1", Name = "《哈利波特-神祕的魔法石》", Price = 100 },
+                new Book() { Id = "2", Name = "《哈利波特-消失的密室》", Price = 100 },
+                new Book() { Id = "3", Name = "《哈利波特-阿茲卡班的逃犯》", Price = 100 },
+                new Book() { Id = "4", Name = "《哈利波特-火盃的考驗》", Price = 100 }
+            };
+
+            var target = new Cart(books);
+            int expected = 320;
+
+            // act
+            var actual = target.GetTotalPrice();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }

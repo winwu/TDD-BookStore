cde2b7c5596c3430e34328e9789fdb3d2d32f8c7
winwu <yiyingwu.1990@gmail.com>
2016-07-12 15:56:11 +0800

    [red] 初始化第一隻測試案例

---
+++ Store/Cart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Store
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class Cart
+    {
+        private List<Book> books;
+
+        public Cart(List<Book> books)
+        {
+            this.books = books;
+        }
+
+        public object GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StoreTests/CartTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod()]
+        public void Get_Total_Price_buy_book_id_1()
+        {
+            //  第一集買了一本，價格應為100*1=190
+            // arrange
+            var books = new List<Book>
+            {
+                new Book()
+                {
+                    Id = "1",
+                    Name = "《哈利波特-神祕的魔法石》",
+                    Price = 100
+                }
+            };
+
+            var target = new Cart(books);
+            int expected = 100;
+
+            // act
+            var actual = target.GetTotalPrice();
+
+            // assert
+            Assert.Fail();
+        }
+    }
+}

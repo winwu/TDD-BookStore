9fd72984c4db3b057755ae1b219f54575532a993
winwu <yiyingwu.1990@gmail.com>
2016-07-12 18:49:19 +0800

    [red] 描述第三個測試案例(第一二三集各買一本  價格應為 100*3*0.9 = 270) 測試失敗

--- StoreTests/CartTests.cs
+++ StoreTests/CartTests.cs
@@ -56,5 +56,27 @@ namespace Store.Tests
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void Get_Total_Price_buy_book_id_1_and_2_and_3()
+        {
+            //  第一二三集各買一本  價格應為 100*3*0.9 = 270
+            // arrange
+            var books = new List<Book>
+            {
+                new Book() { Id = "1", Name = "《哈利波特-神祕的魔法石》", Price = 100 },
+                new Book() { Id = "2", Name = "《哈利波特-消失的密室》", Price = 100 },
+                new Book() { Id = "3", Name = "《哈利波特-阿茲卡班的逃犯》", Price = 100 }
+            };
+
+            var target = new Cart(books);
+            int expected = 270;
+
+            // act
+            var actual = target.GetTotalPrice();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }

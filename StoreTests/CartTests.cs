96ecbb4cda3caee5743531ae50231f6e2c96f53b
winwu <yiyingwu.1990@gmail.com>
2016-07-12 19:06:23 +0800

    [green] 描述第六個測試案例 測試通過

--- StoreTests/CartTests.cs
+++ StoreTests/CartTests.cs
@@ -125,5 +125,28 @@ namespace Store.Tests
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void Get_Total_Price_buy_book_id_1_and_2_and_3_and_3()
+        {
+            //  一二 各買一本  第三買兩本 價格應為 100*3*0.9 + 100 = 370
+            // arrange
+            var books = new List<Book>
+            {
+                new Book() { Id = "1", Name = "《哈利波特-神祕的魔法石》", Price = 100 },
+                new Book() { Id = "2", Name = "《哈利波特-消失的密室》", Price = 100 },
+                new Book() { Id = "3", Name = "《哈利波特-阿茲卡班的逃犯》", Price = 100 },
+                new Book() { Id = "3", Name = "《哈利波特-阿茲卡班的逃犯》", Price = 100 }
+            };
+
+            var target = new Cart(books);
+            int expected = 370;
+
+            // act
+            var actual = target.GetTotalPrice();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
